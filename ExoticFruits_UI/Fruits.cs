26e03101fac551a978c2d43fcf95a585a86f54b9
GianGrassals <gian.grassals@gmail.com>
2021-03-03 00:33:03 -0400

    initial commit, carousel ready

---
+++ ExoticFruits_UI/Fruits.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ExoticFruits_UI
+{
+    public class Fruits
+    {
+
+        public int id { get; set; }
+        public string fruitName { get; set; }
+        public string fruitPrice { get; set; }
+        public string imgSource { get; set; }
+        public string backgroundColor { get; set; }
+
+
+    }
+}
---
+++ ExoticFruits_UI/MainViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace ExoticFruits_UI
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private ObservableCollection<Fruits> Fruits;
+
+        public ObservableCollection<Fruits> fruits
+        {
+            get { return Fruits; }
+            set { Fruits = value ;
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("fruits"));
+                }
+
+            }
+         
+         
+
+        
+        public MainViewModel()
+        {
+            fruits = new ObservableCollection<Fruits>();
+         
+            AddData();
+        }
+
+
+        private void AddData()
+        {
+            fruits.Add(new Fruits
+            {
+                id = 0,
+                fruitName = "Durian Mon Tong",
+                fruitPrice = "$19.99",
+                imgSource = "durian.png",
+                backgroundColor = "#f5e0bc"
+
+            });
+            fruits.Add(new Fruits
+            {
+                id = 1,
+                fruitName = "Hawaiian Papaya",
+                fruitPrice = "$4.55",
+                imgSource = "papaya.png",
+                backgroundColor = "#eb9d63"
+
+            });
+            fruits.Add(new Fruits
+            {
+                id = 1,
+                fruitName = "Kiwi",
+                fruitPrice = "$2.30",
+                imgSource = "kiwi.png",
+                backgroundColor = "#dbde6a"
+
+            });
+        }
+    }
+}
